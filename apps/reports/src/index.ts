522ba60e9bb97648a60643550bb5ebfd7b3b48f7
Eric Masi <emasi@vitusvet.com>
2023-03-02 18:27:02 -0500

    Upload to S3

--- apps/reports/src/index.ts
+++ apps/reports/src/index.ts
@@ -1,10 +1,15 @@
 import { makeTransaction } from "./transactions/transactions";
+import { getBucketName, upload } from "./upload/s3";
 
-(() => {
+(async () => {
   const now = new Date();
   const nowIso = now.toISOString();
   const transactions = Array.from({ length: 10 }).map(() => makeTransaction());
-  const filename = `transactions_report_${nowIso}`;
+  const filename = `transactions_report_${nowIso}.json`;
+  const bucketName = await getBucketName();
+  const body = { reportDate: nowIso, transactions };
 
-  console.log(filename, transactions);
+  await upload(bucketName, filename, JSON.stringify(body));
+
+  console.log("done");
 })();
--- apps/reports/src/upload/s3.ts
+++ apps/reports/src/upload/s3.ts
@@ -1,7 +1,31 @@
-import { S3Client } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  ListBucketsCommand,
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
 
 const client = new S3Client({ region: "us-east-1" });
 
-export function uploadJson() {
-  console.log(typeof client);
+export async function upload(
+  bucketName: string,
+  key: string,
+  body: string | Buffer
+) {
+  const cmd = new PutObjectCommand({
+    Key: key,
+    Bucket: bucketName,
+    Body: body,
+  });
+  await client.send(cmd);
+}
+
+export async function getBucketName() {
+  const { Buckets } = await listBuckets();
+  return (
+    Buckets?.find(({ Name }) => Name?.startsWith("test-files"))?.Name ?? ""
+  );
+}
+
+function listBuckets() {
+  return client.send(new ListBucketsCommand({}));
 }
