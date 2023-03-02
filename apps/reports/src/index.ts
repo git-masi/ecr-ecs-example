924a9465385213846a7d0241886b326b8f977392
Eric Masi <emasi@vitusvet.com>
2023-03-02 18:44:08 -0500

    Upload CSV to S3

--- apps/reports/src/index.ts
+++ apps/reports/src/index.ts
@@ -1,15 +1,23 @@
 import { makeTransaction } from "./transactions/transactions";
 import { getBucketName, upload } from "./upload/s3";
+import papa from "papaparse";
+// import { writeFile } from "fs/promises";
 
 (async () => {
   const now = new Date();
   const nowIso = now.toISOString();
   const transactions = Array.from({ length: 10 }).map(() => makeTransaction());
-  const filename = `transactions_report_${nowIso}.json`;
+  const basename = `transactions_report_${nowIso}`;
   const bucketName = await getBucketName();
   const body = { reportDate: nowIso, transactions };
 
-  await upload(bucketName, filename, JSON.stringify(body));
+  await upload(bucketName, `${basename}.json`, JSON.stringify(body));
 
-  console.log("done");
+  const csv = papa.unparse(transactions, { newline: "\n" });
+
+  console.log(csv);
+
+  await upload(bucketName, `${basename}.csv`, Buffer.from(csv));
+
+  // await writeFile(`data.csv`, Buffer.from(csv));
 })();
