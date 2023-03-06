60a23624be2ec9cb5bcb7703684de469cd261750
Eric Masi <emasi@vitusvet.com>
2023-03-06 14:16:38 -0500

    Add try catch block to log errors to cloudwatch

--- apps/reports/src/index.ts
+++ apps/reports/src/index.ts
@@ -3,18 +3,24 @@ import { getBucketName, upload } from "./upload/s3";
 import papa from "papaparse";
 
 (async () => {
-  const now = new Date();
-  const nowIso = now.toISOString();
-  const transactions = Array.from({ length: 10 }).map(() => makeTransaction());
-  const basename = `transactions_report_${nowIso}`;
-  const bucketName = await getBucketName();
-  const body = { reportDate: nowIso, transactions };
+  try {
+    const now = new Date();
+    const nowIso = now.toISOString();
+    const transactions = Array.from({ length: 10 }).map(() =>
+      makeTransaction()
+    );
+    const basename = `transactions_report_${nowIso}`;
+    const bucketName = await getBucketName();
+    const body = { reportDate: nowIso, transactions };
 
-  await upload(bucketName, `${basename}.json`, JSON.stringify(body));
+    await upload(bucketName, `${basename}.json`, JSON.stringify(body));
 
-  const csv = papa.unparse(transactions, { newline: "\n" });
+    const csv = papa.unparse(transactions, { newline: "\n" });
 
-  await upload(bucketName, `${basename}.csv`, Buffer.from(csv));
+    await upload(bucketName, `${basename}.csv`, Buffer.from(csv));
 
-  console.log(`Finished writing files for ${basename}`);
+    console.log(`Finished writing files for ${basename}`);
+  } catch (error) {
+    console.error(error);
+  }
 })();
