f6616a75a5733c9360cc2de48b6b438e6b64a413
Eric Masi <emasi@vitusvet.com>
2023-03-06 19:18:53 -0500

    Test upload to S3

--- apps/reports/src/index.ts
+++ apps/reports/src/index.ts
@@ -1,6 +1,6 @@
 import { makeTransaction } from "./transactions/transactions";
-// import { getBucketName, upload } from "./upload/s3";
-// import papa from "papaparse";
+import { getBucketName, upload } from "./upload/s3";
+import papa from "papaparse";
 
 (async () => {
   try {
@@ -8,16 +8,14 @@ import { makeTransaction } from "./transactions/transactions";
     const nowIso = now.toISOString();
     const transactions = Array.from({ length: 3 }).map(() => makeTransaction());
     const basename = `transactions_report_${nowIso}`;
-    // const bucketName = await getBucketName();
+    const bucketName = await getBucketName();
     const body = { reportDate: nowIso, transactions };
 
-    // await upload(bucketName, `${basename}.json`, JSON.stringify(body));
+    await upload(bucketName, `${basename}.json`, JSON.stringify(body));
 
-    // const csv = papa.unparse(transactions, { newline: "\n" });
+    const csv = papa.unparse(transactions, { newline: "\n" });
 
-    // await upload(bucketName, `${basename}.csv`, Buffer.from(csv));
-
-    console.log(body);
+    await upload(bucketName, `${basename}.csv`, Buffer.from(csv));
 
     console.log(`Finished writing files for ${basename}`);
   } catch (error) {
